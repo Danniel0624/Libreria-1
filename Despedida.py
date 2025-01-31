bbfc461af4c08a90bc6e9b94cf340fd8115214ef
Danniel0624 <jdlb1728@gmail.com>
2025-01-30 20:48:48 -0500

    Add files via upload

---
+++ Despedida.py
@@ -0,0 +1,14 @@
+
+
+#"Ejercicio 2" 
+
+nombre = "Juan" #str
+edad = 25 #int
+altura = 1.75 #float
+es_estudiante = True #boot
+
+print(f"Nombre: (nombre)")
+print(f"Edad: (edad)")
+print(f"Altura: (altura)m")
+print(f"Es estudiante: (es_estudiante)")
+
