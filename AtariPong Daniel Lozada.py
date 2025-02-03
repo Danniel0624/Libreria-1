924d9dc3488a66d2cab3ec01dfcb857fe9bff62c
Danniel0624 <jdlb1728@gmail.com>
2025-02-03 17:49:26 -0500

    Add files via upload

--- AtariPong Daniel Lozada.py
+++ AtariPong Daniel Lozada.py
@@ -1,2 +1,24 @@
-edad = int(input("¿Cual es tu edad? "))
-if edad
+import pygame
+import sys
+
+# Para inicializar pygame
+pygame.init()
+
+# Configurar pantalla
+screen_width = 800
+screen_height = 600
+screen=pygame.display.set_mode((screen_width, screen_height))
+
+# Colores
+white = (252, 255, 255)
+black = (0, 0, 0)
+
+# Pelota
+ball_size = 20
+ball_speed =[5,5]
+ball = pygame.rect(screen_width // 2 - ball_size // 2, screen_height // 2 - ball_size // 2, ball_size, ball_size)
+
+#Barras
+paddle_width = 10
+paddle_height = 100
+paddle_speed = 5
